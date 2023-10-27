3bb027089e81129ded09a8b3229948a0a73115f8
HL <kalfearislit@gmail.com>
2023-10-28 01:01:55 +0700

    Mengerjakan video 03

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about' , [
+        'name' => "HL" , 
+        "email" => "HL@gmail.com",
+        "image"=> "HL.jpg",
+    ]);
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+
