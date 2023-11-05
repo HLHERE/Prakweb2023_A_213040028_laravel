9e654ea495fa44e5e6a2c14f4aa9e42b9f31a6c7
HL <kalfearislit@gmail.com>
2023-11-05 10:10:46 +0700

    menyelesaikan video ke 08

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -2,6 +2,7 @@
 
 @section('container')
 <article>
+    <p>By . Nakama in <a href="/categories/{{$posts->category->slug}}">{{$posts->category->name}}</a></p>
 <h2>{{$posts-> title }}</h2>
 {!! $posts-> body!!}
 </article>
