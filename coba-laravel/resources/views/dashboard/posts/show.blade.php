accdb75ec5823290638492d7286fc8fb319236dc
HL <kalfearislit@gmail.com>
2023-11-18 00:17:24 +0700

    menyelesaikan video 17

---
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+        <div class="row  my-5">
+            <div class="col-lg-8">
+                <h2 class="mb-3">{{$post-> title }}</h2>
+                <a href="/dashboard/posts" class="btn btn-success">Back to my posts</a> 
+                <a href="" class="btn btn-warning">edit</a> 
+                <a href="" class="btn btn-danger">delete</a> 
+                
+                <img src="https://source.unsplash.com/1200x400?{{$post -> category->name}}" alt="{{$post -> category->name}}" class="img-fluid mt-3">
+
+                <article class="mt-3 fs-5">
+                {!! $post-> body!!}
+                </article>
+
+              
+                <a href="/posts" class = "d-block mt-3">Back to posts</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use Monolog\Formatter\FormatterInterface;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -65,5 +65,10 @@ Route::post('/logout', [LoginController::class,'logout']);
 Route::get('/register', [RegisterController::class,'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class,'store']);
 
-Route::get('/dashboard', [DashboardController::class,'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+} )->middleware('auth');
+
+Route::resource('/dashboard/posts',DashboardPostController::class)
+->middleware('auth');
 
