e1e4089ec29281fc77b759ac9a553058f91c2f3c
HL <kalfearislit@gmail.com>
2023-11-27 15:30:09 +0700

    menyelesaikan video 23

--- coba-laravel/resources/views/dashboard/layouts/sidebar.blade.php
+++ coba-laravel/resources/views/dashboard/layouts/sidebar.blade.php
@@ -32,7 +32,20 @@
                       <button type="submit" class="nav-link px-3"
                         <i class="bi bi-box-arrow-right"></i> Logout</button> 
                     </form>
-              </a>
+                  </a>
+                  @can('admin')
+                  <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-7 mb-2 text-muted">
+                    <span>Administrator</span>
+                  </h6>
+                  <ul class="nav flex-column">
+                    <li class="nav-item">
+                      <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : ''}}" href="/dashboard/categories">
+                        <span data-feather="grid"></span>
+                        Post Categories
+                      </a>
+                    </li>
+                  </ul>
+                  @endcan
             </li>
           </ul>
         </div>
